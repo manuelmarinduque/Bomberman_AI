f85bdc37eef480d7e44d460344524e5d0df46191
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2019-07-28 15:04:09 -0500

    Graficar.py modificado aplicando POO y pygame para graficar el tablero de juego

--- Graficar.py
+++ Graficar.py
@@ -1,32 +1,30 @@
-from tkinter import Label, PhotoImage
+import pygame
+
 
 class Graficar:
 
     def __init__(self):
-        # Labels con las imágenes de los elementos del tablero:
-        self.pared = PhotoImage(file="Images/lad.png")
-        self.agente = PhotoImage(file="Images/mun.png")
-        self.ladrillo = PhotoImage(file="Images/din.png")
-        self.bomba = PhotoImage(file="Images/bom.png")
-        self.puerta = PhotoImage(file="Images/pta.png")
-        self.monstruo1 = PhotoImage(file="Images/mon1.png")
+        self.pared = pygame.image.load("Images/lad.png")
+        self.ladrillo = pygame.image.load("Images/din.png")
+        self.puerta = pygame.image.load("Images/pta.png")
+        self.monstruo = pygame.image.load("Images/mon1.png")
+        self.agente = pygame.image.load("Images/mun.png")
+        self.bomba = pygame.image.load("Images/bom.png")
 
-    def __del__(self):
-        print("borrando objeto")    
-
-    # Función que genera gráficamente el tablero: Recibe como parámetro una matriz o lista de listas:
-    def generar_tablero(self, frame, lista):
-        for n,i in enumerate(lista):
-            for k,j in enumerate(i):
-                    if j == 1: # Pared
-                            Label(frame, image=self.pared, bd=0).grid(row=n, column=k)
-                    if j == 2: # Ladrillo
-                            Label(frame, image=self.ladrillo, bd=0).grid(row=n, column=k)
-                    if j == 4: # Puerta
-                            Label(frame, image=self.puerta, bd=0).grid(row=n, column=k)
-                    if j == 5: # Monstruo
-                            Label(frame, image=self.monstruo1, bd=0).grid(row=n, column=k)
-                    if j == 6: # Agente
-                            Label(frame, image=self.agente, bd=0).grid(row=n, column=k)
-                    if j == 7: # Bomba
-                            Label(frame, image=self.bomba, bd=0).grid(row=n, column=k)
+    def GraficarTablero(self, ventana, matriz):
+        for n, i in enumerate(matriz):
+            posy = n*28
+            for k, j in enumerate(i):
+                posx = k*40
+                if j == 1:  # Pared
+                    ventana.blit(self.pared, (posx, posy))
+                if j == 2:  # Ladrillo
+                    ventana.blit(self.ladrillo, (posx, posy))
+                if j == 4:  # Puerta
+                    ventana.blit(self.puerta, (posx, posy))
+                if j == 5:  # Monstruo
+                    ventana.blit(self.monstruo, (posx, posy))
+                if j == 6:  # Agente
+                    ventana.blit(self.agente, (posx, posy))
+                if j == 7:  # Bomba
+                    ventana.blit(self.bomba, (posx, posy))
